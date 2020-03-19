6a1ac9a365d81ed3ebb1884b50b74a45e7bf83d8
zhixingheyi-tian <41657774+zhixingheyi-tian@users.noreply.github.com>
2020-03-19 09:34:19 +0800

    Add  travis-check.sh

---
+++ oap-cache/oap/travis-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install cmake
+sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install libnuma-dev
+
+# Download spark 2.4.4
+[ -f spark ] || mkdir spark && cd spark && wget http://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz && cd ..
+tar -xf ./spark/spark-2.4.4-bin-hadoop2.7.tgz
+export SPARK_HOME=`pwd`/spark-2.4.4-bin-hadoop2.7
+
+cd /tmp
+git clone https://github.com/memkind/memkind.git
+cd memkind && ./build.sh
+make
+sudo make install
+cd ${TRAVIS_BUILD_DIR}
+
+cd oap-cache/oap/
+mvn clean -q -Ppersistent-memory test
+
