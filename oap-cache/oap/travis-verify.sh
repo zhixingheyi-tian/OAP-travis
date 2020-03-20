d725afcadf65436c0996f257300f333ef5cbed38
zhixingheyi-tian <41657774+zhixingheyi-tian@users.noreply.github.com>
2020-03-20 17:23:19 +0800

    Create travis-verify.sh

---
+++ oap-cache/oap/travis-verify.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+function before_install {
+	sudo apt-get install cmake
+	sudo apt-get install libpthread-stubs0-dev
+	sudo apt-get install libnuma-dev
+}
+
+function install {
+	[ -f spark ] || mkdir spark && cd spark && wget http://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz && cd ..
+	tar -xf ./spark/spark-2.4.4-bin-hadoop2.7.tgz
+	export SPARK_HOME=`pwd`/spark-2.4.4-bin-hadoop2.7
+}
+
+function before_script {
+	cd /tmp
+	git clone https://github.com/memkind/memkind.git
+	cd memkind && ./build.sh
+	make
+	sudo make install
+	cd ${TRAVIS_BUILD_DIR}
+}
+
+function script {
+	cd oap-cache/oap/
+	mvn clean -q -Ppersistent-memory test
+}
+
+if [ "$1" = "before_install" ]; then
+	before_install
+elif [ "$1" = "install" ]; then
+	install
+elif [ "$1" = "before_script" ]; then
+	before_script
+elif [ "$1" = "script" ]; then
+	script
+else
+	echo "Don't support this arg: $1"
+fi
+
